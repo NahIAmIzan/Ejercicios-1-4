67db3e0bd1167be6b832d2a3530d32fbb1ea57c6
NahIAmIzan <imartinezgameiro@gmail.com>
2025-11-19 09:05:53 +0100

    Final

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,3 +3,5 @@ ano = int(input("Introduce un año: "))
 
 if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
     print(f"El año {ano} es bisiesto.")
+else:
+    print(f"El año {ano} no es bisiesto.")
